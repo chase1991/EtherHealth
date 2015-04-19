a2ef2e0ed005a517fea66b0e0e6fcfa4fa1b48d2
qiannanling <lingqiannan@gmail.com>
2015-04-19 17:18:16 -0400

    Setup gem devise

--- EtherHealth/config/application.rb
+++ EtherHealth/config/application.rb
@@ -22,5 +22,8 @@ module EtherHealth
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Required for heroku in the future
+    config.assets.initialize_on_precompile = false 
   end
 end
--- EtherHealth/config/environments/production.rb
+++ EtherHealth/config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for heroku in the future.
+  config.action_mailer.default_url_options = { :host => 'http://PLACEHOLDER' }
 end
